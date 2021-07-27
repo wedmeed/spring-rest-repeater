1e38f1ebf75a8ed00a90ef8904f67b3807c74108
asokolov <wedmeed@mail.ru>
2021-07-27 14:58:57 +0400

    add url repeating

--- src/main/java/ru/wedmeed/research/spring/rest/repeater/service/Repeater.java
+++ src/main/java/ru/wedmeed/research/spring/rest/repeater/service/Repeater.java
@@ -2,6 +2,7 @@ package ru.wedmeed.research.spring.rest.repeater.service;
 
 import ru.wedmeed.research.spring.rest.repeater.web.model.RepeaterRs;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 public class Repeater {
@@ -16,6 +17,14 @@ public class Repeater {
         for (Map.Entry<String, Object> entry : input.entrySet()) {
             response.put(entry.getKey(), entry.getValue());
         }
+    }
+
+    public Repeater (HttpServletRequest request, Map<String, Object> input){
+        response = new RepeaterRs();
+        for (Map.Entry<String, Object> entry : input.entrySet()) {
+            response.put(entry.getKey(), entry.getValue());
+        }
+        response.put("URL", request.getRequestURL());
     }
 
 
--- src/main/java/ru/wedmeed/research/spring/rest/repeater/web/RepeaterController.java
+++ src/main/java/ru/wedmeed/research/spring/rest/repeater/web/RepeaterController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.wedmeed.research.spring.rest.repeater.service.Repeater;
 import ru.wedmeed.research.spring.rest.repeater.web.model.RepeaterRs;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
@@ -14,6 +15,13 @@ public class RepeaterController {
     @GetMapping("/repeat")
     public RepeaterRs repeater(@RequestParam Map<String,Object> input) {
         Repeater repInstance = new Repeater(input);
+        return repInstance.getResponse();
+    }
+
+    @GetMapping("/**")
+    public RepeaterRs repeatWithUrl(HttpServletRequest request,
+                                    @RequestParam Map<String,Object> input) {
+        Repeater repInstance = new Repeater(request, input);
         return repInstance.getResponse();
     }
 }
